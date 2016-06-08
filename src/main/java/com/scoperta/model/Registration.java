b8678427201ca38f83909d298f87335c43c50190
John Hunsley <J00074Hunsle@Avox.local>
2016-06-08 10:31:37 +0100

    Signed-off-by: John Hunsley <J00074Hunsle@Avox.local>

--- src/main/java/com/scoperta/model/Registration.java
+++ src/main/java/com/scoperta/model/Registration.java
@@ -1,9 +1,12 @@
 package com.scoperta.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBVersionAttribute;
+
 import java.io.Serializable;
 
-/**
- *  _______  _______  _______  _______  _______  ______    _______  _______
+/** _______  _______  _______  _______  _______  ______    _______  _______
  * |       ||       ||       ||       ||       ||    _ |  |       ||   _   |
  * |  _____||       ||   _   ||    _  ||    ___||   | ||  |_     _||  |_|  |
  * | |_____ |       ||  | |  ||   |_| ||   |___ |   |_||_   |   |  |       |
@@ -17,7 +20,13 @@ import java.io.Serializable;
  *         Date : 05/06/2016
  *         Time : 09:52
  */
+@DynamoDBTable(tableName="ScopertaRegistrations")
 public class Registration implements Serializable {
     private static final long serialVersionUID = 785794562779985L;
+
+    @DynamoDBVersionAttribute(attributeName = "version")
+    private Long version;
+
+    @DynamoDBHashKey(attributeName="Id")
     private Long id;
 }
